d60c1407974d746c0869fba9eed72e611dd71436
bieul-bonfim <gabrielmmbonfim@gmail.com>
2020-05-12 09:26:00 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateApoointmentService';
+
+const appointmentsRepository = new AppointmentRepository();
+const appointmentsRouter = Router();
+
+// appointmentsRouter.get('/', (request, response) => {
+//   const appointments = appointmentsRepository.all();
+
+//   return response.json(appointments);
+// });
+
+appointmentsRouter.post('/', (request, response) => {
+  try {
+    const { provider_id, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
---
+++ src/services/CreateApoointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider_id, date }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
