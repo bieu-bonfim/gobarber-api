c8b2e969f4730c75e85082a74db2979a40f6abeb
bieul-bonfim <gabrielmmbonfim@gmail.com>
2020-05-15 08:40:57 -0300

    Refatoramento do Código (Appointments)

---
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+
+const sesionsRouter = Router();
+
+sesionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const authenticateUser = new AuthenticateUserService();
+
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sesionsRouter;
