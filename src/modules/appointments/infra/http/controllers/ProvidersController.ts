96603970748df04f7e8e28814bcd82775950bc34
bieul-bonfim <gabrielmmbonfim@gmail.com>
2020-05-25 09:02:51 -0300

    test coverage 100% and day/month availability

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -7,9 +7,9 @@ export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
 
-    const listProvides = container.resolve(ListProvidersService);
+    const listProviders = container.resolve(ListProvidersService);
 
-    const providers = await listProvides.execute({
+    const providers = await listProviders.execute({
       user_id,
     });
 
