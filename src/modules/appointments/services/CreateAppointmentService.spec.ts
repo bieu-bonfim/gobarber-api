28ee875baf7d456f0285b0e84c0bb00d6b62ce12
bieul-bonfim <gabrielmmbonfim@gmail.com>
2020-05-28 16:21:58 -0300

    cache tests and auth elaboration

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,20 +1,25 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 let fakeNotificationsRepository: FakeNotificationsRepository;
 let fakeAppointmentRepository: FakeAppointmentRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let createAppointment: CreateAppointmentService;
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
     fakeNotificationsRepository = new FakeNotificationsRepository();
     fakeAppointmentRepository = new FakeAppointmentRepository();
+    fakeCacheProvider = new FakeCacheProvider();
+
     createAppointment = new CreateAppointmentService(
       fakeAppointmentRepository,
       fakeNotificationsRepository,
+      fakeCacheProvider,
     );
   });
 
--- src/shared/container/providers/CacheProvider/fakes/FakeCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/fakes/FakeCacheProvider.ts
@@ -1,13 +1,39 @@
 import ICacheProvider from '../models/ICacheProvider';
 
-export default class FakeCacheProvider implements ICacheProvider {
-  public async invalidate(key: string): Promise<void> {}
+interface ICacheData {
+  [key: string]: string;
+}
 
-  public async recover(key: string): Promise<string> {
-    return key;
+export default class FakeCacheProvider implements ICacheProvider {
+  private cache: ICacheData = {};
+
+  public async save(key: string, value: string): Promise<void> {
+    this.cache[key] = JSON.stringify(value);
   }
 
-  public async save(key: string, value: string): Promise<void> {}
+  public async recover<T>(key: string): Promise<T | null> {
+    const data = this.cache[key];
 
-  public async invalidatePrefix(prefix: string): Promise<void> {}
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
+
+  public async invalidate(key: string): Promise<void> {
+    delete this.cache[key];
+  }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = Object.keys(this.cache).filter(key =>
+      key.startsWith(`${prefix}:`),
+    );
+
+    keys.forEach(key => {
+      delete this.cache[key];
+    });
+  }
 }
